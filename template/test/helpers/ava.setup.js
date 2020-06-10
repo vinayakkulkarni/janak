6f416862902b8e8636ecc2c55ef98ddba81132ab
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2020-06-10 12:34:06 +0530

    refactor(tests): remove e2e using ava 👀

--- template/test/helpers/ava.setup.js
+++ template/test/helpers/ava.setup.js
@@ -1,25 +1,20 @@
+// https://github.com/vuejs/vue-test-utils/issues/936
+// from @vue/cli-plugin-unit-mocha/setup.js
+require('jsdom-global')();
+// https://github.com/vuejs/vue-test-utils/issues/936
+// better fix for "TypeError: Super expression must either be null or
+// a function" than pinning an old version of prettier.
+window.Date = Date;
+
+// Setup browser environment
 const hooks = require('require-extension-hooks');
+const Vue = require('vue');
 
-if (process.env.TEST === 'unit') {
-  require('jsdom-global')();
-  require('browser-env');
-  const Vue = require('vue');
-  Vue.config.productionTip = false;
-  // https://github.com/nuxt/create-nuxt-app/issues/180#issuecomment-463069941
-  window.Date = global.Date = Date;
-  // https://github.com/video-dev/hls.js/issues/2231#issuecomment-486102552
-  window.Number = Number;
-}
-
-if (process.env.TEST === 'e2e') {
-  const Vue = require('vue');
-  Vue.config.productionTip = false;
-}
+// Setup Vue.js to remove production tip
+Vue.config.productionTip = false;
 
 // Setup vue files to be processed by `require-extension-hooks-vue`
-hooks('vue')
-  .plugin('vue')
-  .push();
+hooks('vue').plugin('vue').push();
 // Setup vue, js and ts files to be processed by `require-extension-hooks-babel`
 hooks(['vue', 'js', 'ts'])
   .exclude(({ filename }) => filename.match(/\/node_modules\//))
