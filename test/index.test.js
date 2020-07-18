88bfb61f39d9bdfec7801f3bfea57e39a79bab05
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2020-07-18 12:54:59 +0530

    refactor(tests): update tests to support different options

---
+++ test/index.test.js
@@ -0,0 +1,43 @@
+import path from 'path'
+import test from 'ava'
+import sao from 'sao'
+import saoConfig from '../saofile'
+
+const generator = path.join(__dirname, '..')
+
+const getPkgFields = (pkg) => {
+  pkg = JSON.parse(pkg)
+  delete pkg.name
+  delete pkg.author
+  delete pkg.version
+  delete pkg.description
+  return pkg
+}
+
+const verifyFileList = async (t, answers = {}) => {
+  const stream = await sao.mock({ generator }, answers)
+  t.snapshot(stream.fileList, 'Generated files')
+}
+
+const verifyPkg = async (t, answers = {}) => {
+  const stream = await sao.mock({ generator }, answers)
+  const pkg = await stream.readFile('package.json')
+  t.snapshot(getPkgFields(pkg), 'package.json')
+}
+
+test('verify default answers', async (t) => {
+  await verifyFileList(t)
+  await verifyPkg(t)
+})
+
+for (const prompt of saoConfig.prompts) {
+  if (Array.isArray(prompt.choices)) {
+    for (const choice of prompt.choices) {
+      test(`Verify ${prompt.name}: ${choice.name}`, async (t) => {
+        const answer = { [prompt.name]: prompt.type === 'checkbox' ? [choice.value] : choice.value }
+        await verifyFileList(t, answer)
+        await verifyPkg(t, answer)
+      })
+    }
+  }
+}
