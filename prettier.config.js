f25bb31822483fe569c2482f468f12fa404c238d
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2021-03-04 21:23:12 +0530

    feat: lockfile v2 🥳

--- prettier.config.js
+++ prettier.config.js
@@ -1,13 +1,3 @@
 module.exports = {
-  $schema: 'http://json.schemastore.org/prettierrc',
-  semi: true,
-  arrowParens: 'always',
-  singleQuote: true,
-  trailingComma: 'all',
-  bracketSpacing: true,
-  htmlWhitespaceSensitivity: 'css',
-  insertPragma: false,
-  tabWidth: 2,
-  useTabs: false,
-  endOfLine: 'lf',
+  ...require('@vinayakkulkarni/prettier-config-vue'),
 };
