16725efe9a3ddec8eedb1b41d13afd6d00c06305
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2021-07-31 19:37:56 +0530

    fix(style): update ESLint config to use `typescript` parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,31 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     es6: true,
     node: true,
   },
-  parser: 'babel-eslint',
-  extends: ['eslint:recommended', 'prettier', 'plugin:prettier/recommended'],
-  plugins: ['prettier'],
-  globals: {
-    use: true,
-    browser: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    lib: ['es2020'],
+    ecmaFeatures: {
+      jsx: true,
+      tsx: true,
+    },
   },
+  plugins: ['jsdoc', '@typescript-eslint', 'prettier'],
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:prettier/recommended',
+    'plugin:jsdoc/recommended',
+    'prettier',
+  ],
+  // add your custom rules here
   rules: {
     'no-console': 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
 };
